28d5332876a00c177c99ffe4a5c7f4f43d42aef9
luisuribemoreira <luisuribemoreira@gmail.com>
2018-08-17 19:11:37 -0300

    Funcionalidad de envio de correo
    -Se implementaron los required para los campos de texto de envio del correo.
    -Se implemento la funcionalidad de envio de correo

--- eatwell/mail.php
+++ eatwell/mail.php
@@ -1,9 +1,22 @@
 <?php
 $nombre = $_POST['nombre'];    
 $asunto = $_POST['asunto'];  
+$fono = $_POST['fono'];
+$tipoE  = $_POST['tipoE'];
+$fecha = $_POST['fecha'];
+$cant = $_POST['cant'];
 $mensaje = $_POST['mensaje'];  
 
-mail('luisuribemoreira@gmail.com',$asunto,$mensaje);
+$mail = "Nombre: ".$nombre .". Fono: ". $fono . ". Tipo de Evento: ".$tipoE . ". Fecha Tentativa: ".$fecha . ". Cantidad de invitados: ".$cant . ". Mensaje: ".$mensaje;
 
-header('Location: index.html');
+if(mail('luisuribemoreira@gmail.com',$asunto,$mail))
+{
+    echo '<script language="javascript">alert("Correo enviado correctamente");</script>'; 
+  
+}else
+{
+    echo '<script language="javascript">alert("El correo no se pudo enviar");</script>'; 
+   
+}
+
 ?>
